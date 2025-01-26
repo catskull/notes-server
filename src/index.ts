9baf7e0753088dffe7c2040f8ef21262ecfda34f
David DeGraw <degraw@fastmail.com>
2025-01-26 16:25:54 -0700

    it commits the code and deploys it to production

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,57 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.json`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+
+import { Octokit, App } from "octokit";
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+
+		// it authenticates the request
+		if (env.KEY !== request.headers.get('key')) {
+			return new Response('u suck', {status: 401})
+		}
+
+		// it receives the parameters
+		const body = await request.json()
+
+		// it removes the double url from apple notes
+		const link = body.link.split('\n')[0];
+
+		// it makes the yaml
+		const yaml =`- note: ${body.note}
+	link: "${link}"
+	date: ${new Date().toLocaleDateString()}
+
+`
+		// it configures the configurations
+		const repo = {
+		  owner: 'catskull',
+		  repo: 'catskull.github.io',
+		  path: '_data/notes.yml',
+		  headers: {
+		    'X-GitHub-Api-Version': '2022-11-28'
+		  }
+		}
+		const octokit = new Octokit({
+			auth: env.TOKEN
+		})
+
+		// it gets the file
+    let file = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {...repo})
+		let content = atob(file?.data?.content ?? '');
+
+		// it rubs the yaml on it's skin
+    const update = await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}',
+    {
+    	...repo,
+    	message: `New note: ${link}`,
+    	content: btoa(yaml + content),
+    	sha: file.data.sha,
+    })
+
+    // it gets the updated file
+    file = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {...repo})
+		content = atob(file?.data?.content ?? '');
+
+		// it shows us the updated file
+		return new Response(content);
 	},
 } satisfies ExportedHandler<Env>;
