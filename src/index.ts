23147d51f48ad7668633f47dee8362c9eac01e2b
David DeGraw <degraw@fastmail.com>
2025-01-26 16:38:37 -0700

    reindent yay

--- src/index.ts
+++ src/index.ts
@@ -17,41 +17,41 @@ export default {
 
 		// it makes the yaml
 		const yaml =`- note: ${body.note}
-	link: "${link}"
-	date: ${new Date().toLocaleDateString()}
+  link: "${link}"
+  date: ${new Date().toLocaleDateString()}
 
-`
+  `
 		// it configures the configurations
-		const repo = {
-		  owner: 'catskull',
-		  repo: 'catskull.github.io',
-		  path: '_data/notes.yml',
-		  headers: {
-		    'X-GitHub-Api-Version': '2022-11-28'
-		  }
-		}
-		const octokit = new Octokit({
-			auth: env.TOKEN
-		})
+  const repo = {
+    owner: 'catskull',
+    repo: 'catskull.github.io',
+    path: '_data/notes.yml',
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  }
+  const octokit = new Octokit({
+   auth: env.TOKEN
+ })
 
 		// it gets the file
-    let file = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {...repo})
-		let content = atob(file?.data?.content ?? '');
+  let file = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {...repo})
+  let content = atob(file?.data?.content ?? '');
 
 		// it rubs the yaml on it's skin
-    const update = await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}',
-    {
-    	...repo,
-    	message: `New note: ${link}`,
-    	content: btoa(yaml + content),
-    	sha: file.data.sha,
-    })
+  const update = await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}',
+  {
+   ...repo,
+   message: `New note: ${link}`,
+   content: btoa(yaml + content),
+   sha: file.data.sha,
+ })
 
     // it gets the updated file
-    file = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {...repo})
-		content = atob(file?.data?.content ?? '');
+  file = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {...repo})
+  content = atob(file?.data?.content ?? '');
 
 		// it shows us the updated file
-		return new Response(content);
-	},
+  return new Response(content);
+},
 } satisfies ExportedHandler<Env>;
